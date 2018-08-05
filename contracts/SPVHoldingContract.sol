5b9d36c79b96afb1a389914727d9361f7eb529b6
sharad-s <sharadshekar@gmail.com>
2018-08-05 16:02:46 -0500

    added voting - Rafi

--- contracts/SPVHoldingContract.sol
+++ contracts/SPVHoldingContract.sol
@@ -1,11 +1,14 @@
 import './Crowdsale.sol';
-
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 contract SPVHoldingContract is Crowdsale {
 
   constructor(uint256 _rate, address _wallet, ERC20 _token) Crowdsale(_rate, _wallet, _token) {
   }
 
   /*  Liquidate */
+  function extractNFT() {
+    require()
+  }
 
-  
+
 }
